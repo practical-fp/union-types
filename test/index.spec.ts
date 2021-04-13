7039ba0b576cdd2c569e57bdff0dc786599f569f
Felix Schorer <felixschorer@users.noreply.github.com>
2021-04-13 10:30:36 +0200

    Restructure tests

--- test/index.spec.ts
+++ test/index.spec.ts
@@ -1,8 +1,10 @@
-import { assert, Has, IsExact } from "conditional-type-checks"
+import { assert, IsExact } from "conditional-type-checks"
 import {
     assertNever,
-    impl,
+    constructor,
+    Constructor,
     hasTag,
+    impl,
     match,
     Narrow,
     predicate,
@@ -11,59 +13,115 @@ import {
     Values,
     Variant,
     WILDCARD,
-    Impl,
-    constructor,
 } from "../src"
 
-type Test = Variant<"Test", number>
-type NotTest = Variant<"NotTest">
-type Union = Test | NotTest
-
-assert<IsExact<Tags<Union>, "Test" | "NotTest">>(true)
-assert<IsExact<Values<Union>, number | undefined>>(true)
-assert<IsExact<Narrow<Union, "Test">, Test>>(true)
-
-interface ExtendedVariant extends Variant<"Test", number> {
-    somethingElse: boolean
-}
-
-assert<IsExact<Narrow<ExtendedVariant | NotTest, "Test">, ExtendedVariant>>(true)
-
-const testArray = new Array<Union>().filter(predicate("Test"))
-assert<IsExact<typeof testArray, Test[]>>(true)
-
-const neverArray = new Array<Union>().filter(predicate("SomethingElse"))
-assert<IsExact<typeof neverArray, never[]>>(true)
-
-const matchResult = match({} as Union, {
-    Test: number => number,
-    NotTest: () => undefined,
-    [WILDCARD]: () => true,
+test("Tags should extract the tag of a variant", () => {
+    type Var = Variant<"1">
+    type Actual = Tags<Var>
+    type Expected = "1"
+    assert<IsExact<Actual, Expected>>(true)
 })
-assert<IsExact<typeof matchResult, number | undefined | boolean>>(true)
 
-assert<IsExact<typeof assertNever, (_: never) => never>>(true)
+test("Tags should extract all tags of a union", () => {
+    type Union = Variant<"1"> | Variant<"2">
+    type Actual = Tags<Union>
+    type Expected = "1" | "2"
+    assert<IsExact<Actual, Expected>>(true)
+})
 
-assert<Has<Impl<Variant<"Test", unknown>>["Test"], <T>(value: T) => Variant<"Test", T>>>(true)
+test("Values should extract the value of a variant", () => {
+    type Var = Variant<"1", number>
+    type Actual = Values<Var>
+    type Expected = number
+    assert<IsExact<Actual, Expected>>(true)
+})
 
-assert<Has<Impl<Variant<"Test", number>>["Test"], <T>(value: T) => Variant<"Test", T>>>(false)
-assert<
-    Has<Impl<Variant<"Test", number>>["Test"], <T extends number>(value: T) => Variant<"Test", T>>
->(true)
-assert<
-    Has<
-        Impl<Variant<"Test", number>>["Test"],
-        <T extends number>(value: T | void) => Variant<"Test", T>
-    >
->(false)
+test("Values should extract all values of a union", () => {
+    type Union = Variant<"1", number> | Variant<"2">
 
-assert<Has<Impl<Variant<"Test">>["Test"], (value: void) => Variant<"Test">>>(true)
-assert<
-    Has<
-        Impl<Variant<"Test", number | undefined>>["Test"],
-        <T extends number | undefined>(value: T | void) => Variant<"Test", T>
-    >
->(true)
+    type Actual = Values<Union>
+    type Expected = number | undefined
+    assert<IsExact<Actual, Expected>>(true)
+})
+
+test("Narrow should narrow a union down to a single Variant", () => {
+    type Var1 = Variant<"1", number>
+    type Var2 = Variant<"2", string>
+    type Union = Var1 | Var2
+
+    type Actual = Narrow<Union, "1">
+    type Expected = Var1
+    assert<IsExact<Actual, Expected>>(true)
+})
+
+test("Narrow should work with extended Variants", () => {
+    interface Var1 extends Variant<"1", number> {
+        someProp: boolean
+    }
+
+    type Var2 = Variant<"2", string>
+    type Union = Var1 | Var2
+
+    type Actual = Narrow<Union, "1">
+    type Expected = Var1
+    assert<IsExact<Actual, Expected>>(true)
+})
+
+test("predicate should be a type guard", () => {
+    type Var1 = Variant<"1", number>
+    type Var2 = Variant<"2", string>
+    type Union = Var1 | Var2
+
+    const result = new Array<Union>().filter(predicate("1"))
+
+    type Actual = typeof result
+    type Expected = Var1[]
+    assert<IsExact<Actual, Expected>>(true)
+})
+
+test("predicate should work with unrelated tags", () => {
+    type Var1 = Variant<"1", number>
+    type Var2 = Variant<"2", string>
+    type Union = Var1 | Var2
+
+    const result = new Array<Union>().filter(predicate("Unrelated"))
+
+    type Actual = typeof result
+    type Expected = never[]
+    assert<IsExact<Actual, Expected>>(true)
+})
+
+test("match should infer the return value", () => {
+    type Union = Variant<"1", number> | Variant<"2">
+
+    const result = match({} as Union, {
+        1: number => number,
+        2: () => undefined,
+        [WILDCARD]: () => true,
+    })
+
+    type Actual = typeof result
+    type Expected = number | undefined | boolean
+    assert<IsExact<Actual, Expected>>(true)
+})
+
+test("the Constructor function should be unbounded generic", () => {
+    type Actual = Constructor<"1", unknown>
+    type Expected = <T>(value: T) => Variant<"1", T>
+    assert<IsExact<Actual, Expected>>(true)
+})
+
+test("the Constructor function should be bounded to number", () => {
+    type Actual = Constructor<"1", number>
+    type Expected = <T extends number>(value: T) => Variant<"1", T>
+    assert<IsExact<Actual, Expected>>(true)
+})
+
+test("the Constructor function argument should be optional", () => {
+    type Actual = Constructor<"1", number | undefined>
+    type Expected = <T extends number | undefined>(value: T | void) => Variant<"1", T>
+    assert<IsExact<Actual, Expected>>(true)
+})
 
 test("tag should tag objects", () => {
     const tagged = tag("Test", { number: 42 })
@@ -120,16 +178,19 @@ test("assertNever should throw an error", () => {
 })
 
 test("constructor should construct a tagged value", () => {
-    const Test = constructor<Union, "Test">("Test")
-    expect(Test(42)).toEqual({ tag: "Test", value: 42 })
+    type Union = Variant<"1", number> | Variant<"2">
+    const ctor = constructor<Union, "1">("1")
+    expect(ctor(42)).toEqual({ tag: "1", value: 42 })
 })
 
 test("impl should construct a tagged value", () => {
+    type Union = Variant<"1", number> | Variant<"2">
     const Union = impl<Union>()
-    expect(Union.Test(42)).toEqual({ tag: "Test", value: 42 })
+    expect(Union[1](42)).toEqual({ tag: "1", value: 42 })
 })
 
 test("impl should construct an empty tagged value", () => {
+    type Union = Variant<"1", number> | Variant<"2">
     const Union = impl<Union>()
-    expect(Union.NotTest()).toEqual({ tag: "NotTest", value: undefined })
+    expect(Union[2]()).toEqual({ tag: "2", value: undefined })
 })
